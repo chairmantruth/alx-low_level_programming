2b0c86f545fd9e5eb03f0cbb61c1159dee594cb3
Prince Owusu Opoku <chairmantruth@gmail.com>
2023-09-11 16:21:23 -0700

    printf not put

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main code
+ * Return: always 0
+ */
+
+int main(void)
+{
+	prinf("with proper grammar, but the outcome is a piece of art", / n);
+	return (0);
+
+}
