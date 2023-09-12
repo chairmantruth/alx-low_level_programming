f5bcf06eeb4aaca9107dd2f3825bc70d9e4be8b9
Prince Owusu Opoku <chairmantruth@gmail.com>
2023-09-11 22:12:58 -0700

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - this is the main function
+ * main - this is the main code
+ *
  * Return: always 0
  */
 
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-        return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+
 }
