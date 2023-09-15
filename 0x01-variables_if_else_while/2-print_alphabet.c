9fc4ae728e04c2c6f1b70b13c650dc03b6ca68ba
Prince Owusu Opoku <chairmantruth@gmail.com>
2023-09-15 09:06:29 -0700

    Numbers

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,12 +5,12 @@
  * Return: Always 0
  *
  * Description: "Print lowercase alphabets"
- *
  */
 
 int main(void)
 {
 	int n = 97;
+
 	while (n <= 122)
 	{
 		putchar(n);
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main code
+ * Description: "Print single b10 digit number"
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+	putchar(n +  48);
+	}
+	putchar('\n');
+	return (0);
+}
