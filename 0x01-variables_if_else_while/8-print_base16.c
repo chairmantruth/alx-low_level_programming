dc3a0e11cc1fb01babb2bde0a96fd08a50f1d40d
Prince Owusu Opoku <chairmantruth@gmail.com>
2023-09-15 09:40:20 -0700

    Hexa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: "base 16 printed"
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (m = 97; m <= 102; m++)
+	{
+		putchar(m);
+	}
+	return (0);
+}
