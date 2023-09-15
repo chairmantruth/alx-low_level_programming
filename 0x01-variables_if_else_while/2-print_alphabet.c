3435801fe2474af014be5c35a3e9a6000d678328
Prince Owusu Opoku <chairmantruth@gmail.com>
2023-09-15 04:29:07 -0700

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - maincode
+ *
+ * Return: Always 0
+ *
+ * Description: "Print lowercase alphabets"
+ *
+ */
+
+int main(void)
+{
+
+	
+	int n = 97;
+	
+	while (n <= 122)
+	{ 
+		putchar(n);
+		n++;
+	}
+	
+	return (0);
+}
