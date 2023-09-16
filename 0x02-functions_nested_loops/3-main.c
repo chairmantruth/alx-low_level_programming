c39e195c36c43b3f366f54dc546d06138e9b6344
Prince Owusu Opoku <chairmantruth@gmail.com>
2023-09-16 06:08:07 -0700

    okay

--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -9,12 +9,12 @@ int main(void)
 {
 	int r;
 
-		r = _islower('H');
-		putchar(r + '0');
-		r = _islower('o');
-		putchar(r + '0');
-		r = _islower(108);
-		putchar(r + '0');
-		putchar('\n');
+	r = _islower('H');
+	putchar(r + '0');
+	r = _islower('o');
+	putchar(r + '0');
+	r = _islower(108);
+	putchar(r + '0');
+	putchar('\n');
 	return (0);
 }
