ffda14ec1fdf9ca6e46e452a814f9966fecc4656
Prince Owusu Opoku <chairmantruth@gmail.com>
2023-09-25 01:21:44 -0700

    main

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -14,5 +14,5 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-int _putchar(char c)
+
 #endif /* MAIN_H */
