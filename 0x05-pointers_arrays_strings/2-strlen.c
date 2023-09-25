1aa0d47f7cf68a227af995fb8ad70efef92c87e3
Prince Owusu Opoku <chairmantruth@gmail.com>
2023-09-24 23:33:18 -0700

    returns the length of a sring

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _strlen - checks the length of a string
+ * @s:input
+ * Return: Always 0 (Success)
+ * Author: IanoNjuguna
+*/
+
+int _strlen(char *s)
+{
+	int length = strlen(s);
+
+	return (length);
+}
