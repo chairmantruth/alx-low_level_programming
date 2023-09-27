d8231d7bd15d3fe6fa8d23d3ce160b15cc746c9e
Prince Owusu Opoku <chairmantruth@gmail.com>
2023-09-27 09:20:52 -0700

    Printing string with new line

---
+++ 0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ * Description: "A function that prints a string followed by a new line"
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	_puts_recursion("Puts with recursion\n");
+	return (0);
+}
+
+
