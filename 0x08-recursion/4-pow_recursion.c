974a83143a4c328e51d02fe1a635a1e5c246a134
Prince Owusu Opoku <chairmantruth@gmail.com>
2023-09-27 22:48:12 -0700

    exponentials

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,20 +1,17 @@
 #include "main.h"
 
 /**
- *_pow_recursion
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: value to raise
+ * @y: power
  *
- * @x: number
- * @y: number
- * Return: the number exonentiated
+ * Return: result of the power
  */
-
 int _pow_recursion(int x, int y)
 {
-	if (y > -1 && y <= 1 && y != 0)
-		return (x);
 	if (y < 0)
 		return (-1);
 	if (y == 0)
 		return (1);
-	return (x * _pow_recursion(x,y - 1);
+	return (x * _pow_recursion(x, y - 1));
 }
