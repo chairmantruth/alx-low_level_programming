f39745923e15baedc4ecc51d052dfe14ecfe5e8d
Prince Owusu Opoku <chairmantruth@gmail.com>
2023-09-27 20:34:08 -0700

    length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns length of a string
+ *
+ * Return: no return
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s = '\0')
+		return (0);
+	else
+	{
+		_strlen_recursion(s);
+	}
