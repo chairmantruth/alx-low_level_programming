2b09b15f73142f417d4727907196c24dbfda0ce9
Prince Owusu Opoku <chairmantruth@gmail.com>
2023-09-27 21:45:11 -0700

    print reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,14 +2,14 @@
 
 /**
  * _print_rev_recursion - reverse string
- *
+ *@s: "string to be printed in reverse"
  * Return:no return
  */
 void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		_print_rev_recursion(s+1);
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
 
 	}
