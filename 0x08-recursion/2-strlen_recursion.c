d16acad6cd32adf5ba8ed3462416513095d9ccec
Brill <57897922+chairmantruth@users.noreply.github.com>
2023-10-16 10:59:20 +0000

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,13 +2,13 @@
 
 /**
  * _strlen_recursion - returns length of a string
- *@s: string to calculate the length
+ *@st: string to calculate the length
  * Return: returns the length of the string
  */
 
-int _strlen_recursion(char *s)
+int _strlen_recursion(char *st)
 {
-	if (*s == '\0')
+	if (*st == '\0')
 		return (0);
-	return (1 + _strlen_recursion(s + 1));
+	return (1 + _strlen_recursion(st + 1));
 }
